87728f9dccaa3c7566f2c34280f25b03bb03d492
SHUBHAM-DEVGEEK <shubhambhardwaj8812@gmail.com>
2026-02-17 21:17:54 +0530

    recursion practice

---
+++ Recursionn.java
@@ -0,0 +1,15 @@
+public class Recursionn {
+    public static void printDec(int n){
+        if(n==10){
+            System.out.println(n);
+            return;
+        }
+     System.out.println(n);
+     printDec(n+1);
+    }
+    public static void main(String args[]){
+        int n=1;
+        printDec(n);
+    }
+    
+}
